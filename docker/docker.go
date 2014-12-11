b4e1cd8474e97721dc38c61eeb5e941dfb5705c7
morpheyesh <morpheyesh@gmail.com>
2014-12-11 15:55:54 +0530

    docker fix

--- docker/docker.go
+++ docker/docker.go
@@ -1,15 +1,18 @@
+ 
 package docker
 
 import (
-	"fmt"
 	log "code.google.com/p/log4go"
 	"encoding/json"
+//	"github.com/tsuru/config"
 	"github.com/megamsys/libgo/db"
 	"github.com/megamsys/gulp/global"
 	"github.com/megamsys/gulp/policies"
 	"github.com/megamsys/gulp/app"
 	"github.com/fsouza/go-dockerclient"
-	//"strings"
+	"strings"
+	"fmt"
+	"bytes"
 )
 
 type Message struct {
@@ -30,14 +33,13 @@ func Handler(chann []byte) error{
 		return err
 	}
 	
-	/*gear, gerr := config.GetString("geard:host")
-	if gerr != nil {
-		return gerr
-	}
+	//gear, gerr := config.GetString("geard:host")
+	//if gerr != nil {
+	//	return gerr
+	//}
+	//s := strings.Split(gear, ":")
+   // geard_host, geard_port := s[0], s[1]
 	
-	s := strings.Split(gear, ":")
-    geard_host, geard_port := s[0], s[1]
-	*/
 	switch req.ReqType {
 	case "create":
 		assemblies := global.Assemblies{Id: req.NodeId}
@@ -59,43 +61,14 @@ func Handler(chann []byte) error{
 		              return asmerr
 	               }				
 	    		   for c := range asm.Components {
-	    		       com := &policies.Component{}
+	    		       com := &global.Component{}
 	    		       mapB, _ := json.Marshal(asm.Components[c])                
                        json.Unmarshal([]byte(string(mapB)), com)
                       
                        if com.Name != "" {
-                          requirements := &policies.ComponentRequirements{}
-	    		          mapC, _ := json.Marshal(com.Requirements)                
-                          json.Unmarshal([]byte(string(mapC)), requirements)
                           
-                          inputs := &policies.ComponentInputs{}
-	    		          mapC, _ = json.Marshal(com.Inputs)                
-                          json.Unmarshal([]byte(string(mapC)), inputs)
-                          
-                          psc, _ := getPredefClouds(requirements.Host)
-                          spec := &global.PDCSpec{}
-                          mapP, _ := json.Marshal(psc.Spec)
-                          json.Unmarshal([]byte(string(mapP)), spec)   
-                       
-                          jso := &docker.Config{Image: inputs.Source}
-                          
-                          
-                          opts := docker.CreateContainerOptions{Name: com.Name, Config: jso}
-                          endpoint := "unix:///var/run/docker.sock"
-			        	  c,_ := docker.NewClient(endpoint)
-			        	  _, err := c.CreateContainer(opts)
-			        	 
-			        	 if err != nil { 
-			        	    log.Error(err)
-			        	    return err
-			        	  }
-			        	 
-
- fmt.Println("-----------============================-----------")			        	
-			        	 
-                  
-			        	 
-
+			        	
+			        	  go createContainer(com)
 			        	  shipperstr += " -c "+ com.Name 
 			         }
                    }
@@ -123,4 +96,53 @@ func getPredefClouds(id string) (*global.PredefClouds, error) {
 	return pre, nil
 }
 
+func createContainer(com *global.Component) error {
+	endpoint := "unix:///var/run/docker.sock"
+	client, _ := docker.NewClient(endpoint)
+                        
+    var buf bytes.Buffer
+    source := strings.Split(com.Inputs.Source, ":")
+    var tag string
+    
+    if len(source) > 1 {
+    	   tag = source[1]
+    	    
+     } else {
+    	 tag = ""
+    } 
+    
+    
+	opts := docker.PullImageOptions{
+	                    Repository:   source[0],
+	                    Registry:     "",
+	                    Tag:          tag,
+	                    OutputStream: &buf,
+	                   }
+	pullerr := client.PullImage(opts, docker.AuthConfiguration{})
+	if pullerr != nil {
+	     log.Error(pullerr)
+    }
 
+    config := docker.Config{Image: "gomegam/megamgateway:0.5.0"}
+	copts := docker.CreateContainerOptions{Name: "redis", Config: &config}
+	container, conerr := client.CreateContainer(copts)
+    fmt.Println("++++++++++++++++++++++++++++++++++++++++++++")
+	if conerr != nil {
+	     log.Error(conerr)
+	}
+	
+	   cont := &docker.Container{}
+           mapP, _ := json.Marshal(container)
+           json.Unmarshal([]byte(string(mapP)), cont)  
+	
+	
+	serr := client.StartContainer(cont.ID, &docker.HostConfig{})
+	if serr != nil {
+		log.Error(serr)
+	}
+	   
+    contt, _ := client.ListContainers(docker.ListContainersOptions{})
+    fmt.Println("--------------------------");
+    fmt.Println(contt)	
+    return nil
+}
