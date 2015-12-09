5e566ec851c1ca8aabecae35e0b091a16c34a7a8
indykish <nkishore@megam.io>
2015-12-09 10:44:34 +0530

    Refreshed libgo to Dec 8 latest version

--- db/riak.go
+++ db/riak.go
@@ -1,35 +1,34 @@
 package db
 
 import (
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"github.com/megamsys/libgo/db"
 	"github.com/megamsys/libgo/hc"
-
+	"github.com/megamsys/libgo/cmd"
 	"github.com/megamsys/gulp/meta"
 )
 
 func init() {
-	hc.AddChecker("Riak", healthCheck)
+	hc.AddChecker("megamd:riak", healthCheck)
 }
 
-func healthCheck() error {
+func healthCheck() (interface{}, error) {
 	conn, err := newConn("test")
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer conn.Close()
-	return nil
+	return fmt.Sprintf("%s up", meta.MC.Riak), nil
 }
 
 //A global function which helps to avoid passing config of riak everywhere.
 func newConn(bkt string) (*db.Storage, error) {
-	log.Debug("New bucket: " + bkt)
 	r, err := db.NewRiakDB(meta.MC.Riak, bkt)
-	log.Debug(r)
-	//r, err := db.NewRiakDB([]string{"localhost:8087"}, bkt)
 	if err != nil {
 		return nil, err
 	}
+
 	return r.Conn()
 }
 
@@ -39,7 +38,8 @@ func Fetch(bkt string, key string, data interface{}) error {
 		return err
 	}
 	defer s.Close()
-	log.Debugf("  [riak] fetch (%s,%s)", bkt, key)
+	log.Debugf("%s (%s, %s)", cmd.Colorfy("  > [riak] fetch", "blue", "", "bold"), bkt, key)
+
 	if err = s.FetchStruct(key, data); err != nil {
 		return err
 	}
@@ -62,12 +62,28 @@ func FetchObject(bkt string, key string) (string, error) {
 
 func Store(bkt string, key string, data interface{}) error {
 	s, err := newConn(bkt)
+
 	if err != nil {
 		return err
 	}
 	defer s.Close()
-	log.Debugf("  [riak] store (%s,%s)", bkt, key)
+	log.Debugf("%s (%s, %s)", cmd.Colorfy("  > [riak] store", "blue", "", "bold"), bkt, key)
+
 	if err = s.StoreStruct(key, data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Delete(bkt string, key string) error {
+	s, err := newConn(bkt)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+	log.Debugf("%s (%s, %s)", cmd.Colorfy("  > [riak] delete", "blue", "", "bold"), bkt, key)
+
+	if err = s.DeleteObject(key); err != nil {
 		return err
 	}
 	return nil
