24ffc97ac4ca41690e3bf10b3388fcc0b1465dd9
indykish <nkishore@megam.io>
2016-01-11 14:44:52 +0530

    Added testing for boot chefrepo stuff

---
+++ provision/chefsolo/chefrepo_test.go
@@ -0,0 +1,31 @@
+package chefsolo
+
+import (
+	"io/ioutil"
+
+ 	"github.com/megamsys/gulp/meta"
+	"gopkg.in/check.v1"
+)
+
+
+func (s *S) TestDownload(c *check.C) {
+	c.Assert(s.config, check.NotNil)
+	c.Assert(s.M, check.NotNil)
+	c.Assert(meta.MC.Dir, check.NotNil)
+	d := ioutil.Discard
+	cr := NewChefRepo(s.M, d)
+	c.Assert(cr, check.NotNil)
+	err := cr.Download(true)
+	c.Assert(err, check.IsNil)
+}
+
+func (s *S) TestTorr(c *check.C) {
+	c.Assert(s.config, check.NotNil)
+	c.Assert(s.M, check.NotNil)
+	cr := NewChefRepo(s.M, ioutil.Discard)
+	c.Assert(cr, check.NotNil)
+	err := cr.Download(true)
+	c.Assert(err, check.IsNil)
+	err = cr.Torr()
+	c.Assert(err, check.IsNil)
+}
--- provision/chefsolo/provisioner_test.go
+++ provision/chefsolo/provisioner_test.go
@@ -18,17 +18,33 @@ package chefsolo
 
 import (
 	"testing"
-
 	"gopkg.in/check.v1"
+	"github.com/megamsys/gulp/meta"
 )
 
 func Test(t *testing.T) {
 	check.TestingT(t)
 }
 
-type S struct{}
+type S struct {
+	config *meta.Config
+	M      map[string]string
+}
 
 var _ = check.Suite(&S{})
+
+func (s *S) SetUpSuite(c *check.C) {
+	cfg := meta.NewConfig()
+	cfg.MkGlobal()
+	s.config = cfg
+	m := make(map[string]string)
+	m[CHEFREPO_GIT] = "https://github.com/megamsys/chef-repo.git"
+	m[CHEFREPO_TARBALL] = "https://github.com/megamsys/chef-repo/archive/0.94.tar.gz"
+	s.M = m
+	c.Assert(s.config, check.NotNil)
+  c.Assert(s.M, check.NotNil)
+
+}
 
 /*
 func (s *S) TestPrepareFiles(c *check.C) {
