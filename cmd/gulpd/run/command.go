1af0b12c6da856bf9719e0d6b11f084c22e7d41a
vijaykanthm28 <mvijaykanth@megam.io>
2016-09-23 15:58:10 +0530

    version file added

--- cmd/gulpd/run/command.go
+++ cmd/gulpd/run/command.go
@@ -1,5 +1,5 @@
 /*
-** Copyright [2013-2015] [Megam Systems]
+** Copyright [2013-2016] [Megam Systems]
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -66,7 +66,7 @@ func (c *Start) Run(context *cmd.Context) error {
 		return fmt.Errorf("parse config: %s", err)
 	}
 	cmd := NewCommand()
-	cmd.Version = "0.9.2"
+	cmd.Version = "1.5-1"
 
 	if err := cmd.Gpd(config, cmd.Version); err != nil {
 		return fmt.Errorf("run: %s", err)
