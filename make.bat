c54e7aa7bb8d9e15f982377afa6ba124ea32da1d
rathishvbr <rathishvbr@outlook.com>
2017-02-28 20:25:06 +0530

    Make bat file for windows.

---
+++ make.bat
@@ -0,0 +1,8 @@
+set GOPATH  = %MEGAM_GOPATH%:%CD%\..\..\..\..\
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+echo %datetimef%
+echo %GOPATH%
+rm -rf %MEGAM_GOPATH%
+go get %GO_EXTRAFLAGS% -u -d -t -insecure ./...
+rm -f gulpd
+go build %GO_EXTRAFLAGS% -o gulpd.exe ./cmd/gulpd
