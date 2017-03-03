cbba4c62d72183eed20febc655ba6b4e5b840256
rathishvbr <rathishvbr@outlook.com>
2017-03-03 10:25:51 +0530

    Commands are added in the maka bat file.

--- make.bat
+++ make.bat
@@ -1,4 +1,7 @@
 set GOPATH  = %MEGAM_GOPATH%:%CD%\..\..\..\..\
+
+:: MEGAM_GOPATH This is where gulp project placed.
+
 set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
 echo %datetimef%
 echo %GOPATH%
